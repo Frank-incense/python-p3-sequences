ea48636eb78e96cfa205da9504c17cfbc6be6ae8
Frankincense <frankincensewelsey@gmail.com>
2025-05-07 13:25:15 +0300

    Created a fibonnaci sequenze and printed it

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = getfib(length)
+    print(fibonacci_list)
+
+def getfib(length):
+    fibonacci =[]
+    for i in range(0,length):
+        if i == 0 or i == 1:
+            fibonacci.append(i)
+        else:
+            fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
+    return fibonacci
